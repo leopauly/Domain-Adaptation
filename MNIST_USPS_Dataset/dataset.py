96515fedfd1f661eb78f7de086fdc7c60aed45f2
R S Stone <sc16rsmy@login1.arc3.leeds.ac.uk>
2017-10-24 15:57:21 +0100

    basic algorithm for iterative training complete

--- MNIST_USPS_Dataset/dataset.py
+++ MNIST_USPS_Dataset/dataset.py
@@ -109,11 +109,20 @@ class DataSet(object):
         new_label[max_preds[i]] = 1
         labels_.append(new_label)
 
+    starting_index = 0
+    if(self._num_images == 0):
+        starting_index = 1
+        # There's probably a better way to do this, but here we are manually adding the first element to allow for concatenation.
+        self._images = [images_[0]]
+        self._cls = [cls_[0]]
+        self._set_ids = [set_ids_[0]]
+        self._labels = [labels_[0]]
+        
     # Add all the data.
-    self._images = np.concatenate((self._images, images_))
-    self._cls = np.concatenate((self._cls, cls_))
-    self._set_ids = np.concatenate((self._set_ids, set_ids_))
-    self._labels = np.concatenate((self._labels, labels_))
+    self._images = np.concatenate((self._images, images_[starting_index:]))
+    self._cls = np.concatenate((self._cls, cls_[starting_index:]))
+    self._set_ids = np.concatenate((self._set_ids, set_ids_[starting_index:]))
+    self._labels = np.concatenate((self._labels, labels_[starting_index:]))
     self._num_images += num_added
 
     # Reshuffle everything the same way.
@@ -147,48 +156,59 @@ class DataSet(object):
 
     return self._images[start:end], self._labels[start:end] # self._cls[start:end], self._set_ids[start:end]
 
-def generate_combined_dataset(mnist_dataset, usps_dataset):
-	class DataSets(object):
-		pass
-	dataset = DataSets()
+def generate_combined_dataset(source, target):
+    ''' The data object for tri-training includes four different DataSet classes:
+            1. data.test - target.test only
+            2. data.pseudo - pseudo-labeled target training images
+            3. data.train - data.pseudo + source images
+            4. data.target - unlabeled target training images
+    '''
+    class DataSets(object):
+        pass
+    dataset = DataSets()
 
-	# Complete MNIST
-	dataset.train = DataSet(np.concatenate( \
-		(mnist_dataset.train.images, mnist_dataset.valid.images, mnist_dataset.test.images)), \
-		np.concatenate((mnist_dataset.train.cls, mnist_dataset.valid.cls, mnist_dataset.test.cls)), \
-		np.concatenate((mnist_dataset.train.set_ids, mnist_dataset.valid.set_ids, mnist_dataset.test.set_ids)), \
-		)
-	dataset.test = DataSet(usps_dataset.test.images, usps_dataset.test.cls, usps_dataset.test.set_ids)
+    # Complete MNIST
+    dataset.train = DataSet(np.concatenate( \
+                                           (source.train.images, source.valid.images, source.test.images)), \
+                            np.concatenate((source.train.cls, source.valid.cls, source.test.cls)), \
+                            np.concatenate((source.train.set_ids, source.valid.set_ids, source.test.set_ids)), \
+                           )
+    dataset.test = DataSet(target.test.images, target.test.cls, target.test.set_ids)
+    dataset.pseudo = DataSet(np.array([]), np.array([]), np.array([])) # Starts out empty.
 
-	# Only used to keep track of remaining USPS images not yet added in bootstrap iterations.
-	dataset.usps_train = DataSet(usps_dataset.train.images, usps_dataset.train.cls, usps_dataset.train.set_ids)
-	return dataset
+    # Only used to keep track of remaining USPS images not yet added in bootstrap iterations.
+    dataset.target = DataSet(target.train.images, target.train.cls, target.train.set_ids)
+    return dataset
+
 
 def read_datasets():
-	class DataSets(object):
-		pass
-	mnist_datasets = DataSets()
-	usps_datasets = DataSets()
+    ''' This is just for MNIST and USPS.
+        TODO: needs to have options for changing the dataset combination. 
+     '''
+    class DataSets(object):
+        pass
+    mnist_datasets = DataSets()
+    usps_datasets = DataSets()
 
-	start_time = time.time()
+    start_time = time.time()
 
-	# mnist_x[i]; 0 = train, 1 = valid, 2 = test
-	mnist_x, usps_x, mnist_y, usps_y = mnus.dataset(normalisation=True, store=False)
+    # mnist_x[i]; 0 = train, 1 = valid, 2 = test
+    mnist_x, usps_x, mnist_y, usps_y = mnus.dataset(normalisation=True, store=False)
 
-	mnist_datasets.train = DataSet(mnist_x[0], mnist_y[0], np.zeros((len(mnist_y[0]))))
-	mnist_datasets.valid = DataSet(mnist_x[1], mnist_y[1], np.zeros((len(mnist_y[1]))))
-	mnist_datasets.test = DataSet(mnist_x[2], mnist_y[2], np.zeros((len(mnist_y[2]))))
+    mnist_datasets.train = DataSet(mnist_x[0], mnist_y[0], np.zeros((len(mnist_y[0]))))
+    mnist_datasets.valid = DataSet(mnist_x[1], mnist_y[1], np.zeros((len(mnist_y[1]))))
+    mnist_datasets.test = DataSet(mnist_x[2], mnist_y[2], np.zeros((len(mnist_y[2]))))
 
-	# For now, the USPS train includes the validation set, so:
-	# usps_x[i]; 0 = train, 1 = test
-	usps_datasets.train = DataSet(usps_x[0], usps_y[0], np.ones((len(usps_y[0]))))
-	usps_datasets.test = DataSet(usps_x[1], usps_y[1], np.ones((len(usps_y[1]))))
+    # For now, the USPS train includes the validation set, so:
+    # usps_x[i]; 0 = train, 1 = test
+    usps_datasets.train = DataSet(usps_x[0], usps_y[0], np.ones((len(usps_y[0]))))
+    usps_datasets.test = DataSet(usps_x[1], usps_y[1], np.ones((len(usps_y[1]))))
 
 
-	end_time = time.time()
-	time_dif = end_time - start_time
+    end_time = time.time()
+    time_dif = end_time - start_time
 
-	# print("Time elapsed: " + str(timedelta(seconds=int(round(time_dif)))))
-	return mnist_datasets, usps_datasets
+    # print("Time elapsed: " + str(timedelta(seconds=int(round(time_dif)))))
+    return mnist_datasets, usps_datasets
 
 
